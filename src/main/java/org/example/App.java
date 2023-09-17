0b1b81da48f86d9c899c3c7b8cd733f4d6102f9d
pjonastik <peto.jonastik@gmail.com>
2023-09-17 22:25:25 +0200

    B2 - task8: add me (Jan Mrkvicka)
    Jan Mrkvicka created this commit personally!

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("In this class are following superstars:");
+        System.out.println("Jan Mrkvicka");
     }
 }
