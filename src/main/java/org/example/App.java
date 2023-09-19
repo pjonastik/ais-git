57d45ea4a90c0ed86e49a14a42fbd1aec229beb8
LukasRotrekl <lukas.rotrekl@ais-servis.cz>
2023-09-19 14:42:50 +0200

    lukas rotrekl vypsani do konzole

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("In this class are following superstars:");
+        System.out.println("Lukas Rotrekl");
     }
 }
